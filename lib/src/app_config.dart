e2c95b3285e864c67ae939eadeb35a342fa1e15f
Farhan Anggawie <farhan.anggawie@gmail.com>
2023-08-21 17:29:02 +0700

    enable config from json file

--- lib/src/app_config.dart
+++ lib/src/app_config.dart
@@ -11,24 +11,17 @@ class AppConfig {
     // set default to dev if nothing was passed
     env = env ?? 'dev';
 
-    // hardcoded for now
-    const supabaseUrl = "https://stukaubuhnosgjmtblwh.supabase.co";
-    const supabaseAnonKey =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0dWthdWJ1aG5vc2dqbXRibHdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg5NTk2MzQsImV4cCI6MjAwNDUzNTYzNH0.0X8sdHTS_m2FyfwyT2nGsOSgcLbk8yWkwJ6Zdep2IJw";
-    return AppConfig(
-        supabaseUrl: supabaseUrl, supabaseAnonKey: supabaseAnonKey);
-
     // load the json file
-    // final contents = await rootBundle.loadString(
-    //   'assets/config/.env.$env.json',
-    // );
+    final contents = await rootBundle.loadString(
+      'assets/config/.env.$env.json',
+    );
 
     // decode our json
-    // final json = jsonDecode(contents);
+    final json = jsonDecode(contents);
 
     // convert our JSON into an instance of our AppConfig class
-    // return AppConfig(
-    //     supabaseUrl: json['supabase']['url'],
-    //     supabaseAnonKey: json['supabase']['anonKey']);
+    return AppConfig(
+        supabaseUrl: json['supabase']['url'],
+        supabaseAnonKey: json['supabase']['anonKey']);
   }
 }
